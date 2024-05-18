854e7cd654cb11f3d8119d9196cf4f2268d35cd8
Ilya Hamisoniya <Ildoradogam@gmail.com>
2024-05-18 19:17:25 +0200

    initiate
    feat: install libraries

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ProductForm from "./components/ProductForm";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Product Information Form</h1>
+      <ProductForm />
+    </div>
+  );
+};
+
+export default App;
